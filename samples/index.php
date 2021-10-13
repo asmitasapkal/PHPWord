185ccdd38ba56d42372bae7ef9d164d29f0825c3
asmita <asmitasapkal111@gmail.com>
2021-10-13 14:02:14 +0530

    something new

--- samples/index.php
+++ samples/index.php
@@ -2,16 +2,13 @@
 include_once 'Sample_Header.php';
 
 use PhpOffice\PhpWord\Settings;
-
+bhvdhvfhs
 $requirements = array(
     'php'   => array('PHP 5.3.3', version_compare(PHP_VERSION, '5.3.3', '>=')),
     'xml'   => array('PHP extension XML', extension_loaded('xml')),
     'temp'  => array('Temp folder "<code>' . Settings::getTempDir() . '</code>" is writable', is_writable(Settings::getTempDir())),
     'zip'   => array('PHP extension ZipArchive (optional)', extension_loaded('zip')),
-    'gd'    => array('PHP extension GD (optional)', extension_loaded('gd')),
-    'xmlw'  => array('PHP extension XMLWriter (optional)', extension_loaded('xmlwriter')),
-    'xsl'   => array('PHP extension XSL (optional)', extension_loaded('xsl')),
-);
+   );
 if (!CLI) {
     ?>
 <div class="jumbotron">
